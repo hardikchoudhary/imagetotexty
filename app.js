42c86b7430f755710171427ef165420b669312e9
hardikchoudhary <hardik.choudhary123@gmail.com>
2017-07-25 10:48:35 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,160 +1,137 @@
-//var yellowLineData = ["Samaypur Badli"
-//    , "Rohini Sector 18, 19"
-//    , "Haiderpur Badli Mor"
-//    , "Jahangirpuri"
-//    , "Adarsh Nagar"
-//    , "Azadpur"
-//    , "Model Town"
-//    , "GTB Nagar"
-//    , "Viswavidyalaya"
-//    , "Vidhan Sabha"
-//    , "Civil Lines"
-//    , "Kashmere Gate "
-//    , "Chandhni Chowk"
-//    , "Chawri Bazar"
-//    , "NewDelhi"
-//    , "Rajiv Chowk"
-//    , "Patel Chowk"
-//    , "Central Secretariat"
-//    , "Udyog Bhawan"
-//    , "Lok Kalyan Marg"
-//    , "Jorbagh"
-//    , "INA"
-//    , "AIIMS"
-//    , "Green Park"
-//    , "Hauz Khas"
-//    , "MalviaNagar"
-//    , "Saket"
-//    , "Qutab Minar"
-//    , "Chhattarpur"
-//    , "Sultanpur"
-//    , "Ghotorni"
-//    , "Arjan Garh"
-//    , "Gurudronacharya"
-//    , "Sikandarpur"
-//    , "MG Road"
-//    , "IFFCO Chowk"
-//    , "Huda City Centre"];
+// This loads the environment variables from the .env file
+require('dotenv-extended').load();
 
-//var blueLineData = [
-//    "Noida City Centre"
-//    , "Golf Course      "
-//    , "Botanical Garden "
-//    , "Noida Sect 18    "
-//    , "Noida Sect 16    "
-//    , "Noida Sect 15    "
-//    , "New Ashok Nagar  "
-//    , "Mayur Vihar Ext  "
-//    , "Mayur Vihar- I   "
-//    , "Akshardham       "
-//    , "Yamuna Bank      "
-//    , "Indraprasta      "
-//    , "Pragati Maidan"
-//    , "Mandi House"
-//    , "Barakhamba"
-//    , "Rajiv Chowk"
-//    , "RK Ashram Marg"
-//    , "Jhandewalan"
-//    , "Karol Bagh"
-//    , "Rajendra Place"
-//    , "Patel Nagar"
-//    , "Shadi Pur"
-//    , "Kirti Nagar"
-//    , "Moti Nagar"
-//    , "Ramesh Nagar"
-//    , "Rajouri Garden"
-//    , "Tagore Garden"
-//    , "Subash Nagar"
-//    , "Tilak Nagar"
-//    , "Janak Puri East"
-//    , "Janak Puri West"
-//    , "Uttam Nagar East"
-//    , "Uttam Nagar West "
-//    , "Nawada           "
-//    , "Dwaraka Mor      "
-//    , "Dwarka           "
-//    , "Dwarka Sector - 14"
-//    , "Dwarka Sector - 13"
-//    , "Dwarka Sector - 12"
-//    , "Dwarka Sector - 11"
-//    , "Dwarka Sector - 10"
-//    , "Dwarka Sector - 9 "
-//    , "Dwarka Sector - 8 "
-//    , "Dwarka Sector - 21"
-//];
+var builder = require('botbuilder'),
+    needle = require('needle'),
+    restify = require('restify'),
+    url = require('url'),
+    validUrl = require('valid-url'),
+    captionService = require('./caption-service');
+
+//=========================================================
+// Bot Setup
+//=========================================================
 
 // Setup Restify Server
-
-// var makeFunc = require('./getLongLatitude.js');
-
-	var NodeGeocoder = require('node-geocoder');
- 
-
-
-// myParams is parameters that you will pass to this function
-
-
-var restify = require('restify');
-
-
 var server = restify.createServer();
-var builder = require('botbuilder');
-var passingresult="";
-var assert = require("assert");
-
-var fs = require('fs');
-
-
-
-
-
-var yellowLineData1 = ["Samaypur Badli", "Jahangirpuri", "Change Red Line Kashmere Gate", "Change Orange Line New Delhi", "Chawri Bazaar", "Change Blue Line Rajiv Chowk metro", "Change  Violet Line Central Secretariat", "Qutub Minar", "Chhatarpur", "Change Rapid MetroRail Line Sikanderpur", "HUDA City Centre"];
-
-var blueLineData1 = ["change Orange Line Dwarka Sector 21", "Dwarka Sub City", "Dwarka Sector 9", "Change Green Line Kirti Nagar", "Change Line yellow Rajiv Chowk","Change Violet line Mandi House","Barakhamba Road", "Pragati Maidan","Indraprastha", "Change Vaishali Line Yamuna Bank","Noida City Centre"];
 server.listen(process.env.port || process.env.PORT || 3978, function () {
-   console.log('%s listening to %s', server.name, server.url); 
-
+    console.log('%s listening to %s', server.name, server.url);
 });
 
-// Create chat connector for communicating with the Bot Framework Service
+// Create chat bot
 var connector = new builder.ChatConnector({
-    appId: "3dfa0867-614d-40d2-8092-08fd56df09f6",
-    appPassword:"mHCEFZUbfZnFH5HLUseuJK1"
+    appId: process.env.MICROSOFT_APP_ID,
+    appPassword: process.env.MICROSOFT_APP_PASSWORD
 });
 
-var redJn=["Kashimiri Gate","Inderlok"]
-var blueJn=["Rajiv Chowk","Kirti Nagar"];
-var violetJn=["Central Secretariat","Kashimiri Gate"];
-var orangeJn=["Dwarka Sector 21","New Delhi"];
-var yellowjn=["Rajiv Chowk","Central Secretariat","Kashimiri Gate","New Delhi"];
-var greenJn=["Kirti Nagar"];
-
-
-// Listen for messages from users 
 server.post('/api/messages', connector.listen());
 
-// Send welcome when conversation with bot is started, by initiating the root dialog
-var bot = new builder.UniversalBot(connector);
-
-
-// Create your bot with a function to receive messages from the user
-bot = new builder.UniversalBot(connector, function (session) {
-    var msg = session.message;
-    if (msg.attachments && msg.attachments.length > 0) {
-     // Echo back attachment
-     var attachment = msg.attachments[0];
-        session.send({
-            text: "You sent:",
-            attachments: [
-                {
-                    contentType: attachment.contentType,
-                    contentUrl: attachment.contentUrl,
-                    name: attachment.name
-                }
-            ]
-        });
+// Gets the caption by checking the type of the image (stream vs URL) and calling the appropriate caption service method.
+var bot = new builder.UniversalBot(connector, function (session) {
+    if (hasImageAttachment(session)) {
+        var stream = getImageStreamFromMessage(session.message);
+        captionService
+            .getCaptionFromStream(stream)
+            .then(function (caption) { handleSuccessResponse(session, caption); })
+            .catch(function (error) { handleErrorResponse(session, error); });
     } else {
-        // Echo back users text
-        session.send("You said: %s", session.message.text);
+        var imageUrl = parseAnchorTag(session.message.text) || (validUrl.isUri(session.message.text) ? session.message.text : null);
+        if (imageUrl) {
+            captionService
+                .getCaptionFromUrl(imageUrl)
+                .then(function (caption) { handleSuccessResponse(session, caption); })
+                .catch(function (error) { handleErrorResponse(session, error); });
+        } else {
+            session.send('Did you upload an image? I\'m more of a visual person. Try sending me an image or an image URL');
+        }
     }
 });
+
+//=========================================================
+// Bots Events
+//=========================================================
+
+//Sends greeting message when the bot is first added to a conversation
+bot.on('conversationUpdate', function (message) {
+    if (message.membersAdded) {
+        message.membersAdded.forEach(function (identity) {
+            if (identity.id === message.address.bot.id) {
+                var reply = new builder.Message()
+                    .address(message.address)
+                    .text('Hi! I am ImageCaption Bot. I can understand the content of any image and try to describe it as well as any human. Try sending me an image or an image URL.');
+                bot.send(reply);
+            }
+        });
+    }
+});
+
+
+//=========================================================
+// Utilities
+//=========================================================
+function hasImageAttachment(session) {
+    return session.message.attachments.length > 0 &&
+        session.message.attachments[0].contentType.indexOf('image') !== -1;
+}
+
+function getImageStreamFromMessage(message) {
+    var headers = {};
+    var attachment = message.attachments[0];
+    if (checkRequiresToken(message)) {
+        // The Skype attachment URLs are secured by JwtToken,
+        // you should set the JwtToken of your bot as the authorization header for the GET request your bot initiates to fetch the image.
+        // https://github.com/Microsoft/BotBuilder/issues/662
+        connector.getAccessToken(function (error, token) {
+            var tok = token;
+            headers['Authorization'] = 'Bearer ' + token;
+            headers['Content-Type'] = 'application/octet-stream';
+
+            return needle.get(attachment.contentUrl, { headers: headers });
+        });
+    }
+
+    headers['Content-Type'] = attachment.contentType;
+    return needle.get(attachment.contentUrl, { headers: headers });
+}
+
+function checkRequiresToken(message) {
+    return message.source === 'skype' || message.source === 'msteams';
+}
+
+/**
+ * Gets the href value in an anchor element.
+ * Skype transforms raw urls to html. Here we extract the href value from the url
+ * @param {string} input Anchor Tag
+ * @return {string} Url matched or null
+ */
+function parseAnchorTag(input) {
+    var match = input.match('^<a href=\"([^\"]*)\">[^<]*</a>$');
+    if (match && match[1]) {
+        return match[1];
+    }
+
+    return null;
+}
+
+//=========================================================
+// Response Handling
+//=========================================================
+function handleSuccessResponse(session, caption) {
+    if (caption) {
+        session.send('I think it\'s ' + caption);
+    }
+    else {
+        session.send('Couldn\'t find a caption for this one');
+    }
+
+}
+
+function handleErrorResponse(session, error) {
+    var clientErrorMessage = 'Oops! Something went wrong. Try again later.';
+    if (error.message && error.message.indexOf('Access denied') > -1) {
+        clientErrorMessage += "\n" + error.message;
+    }
+
+    console.error(error);
+    session.send(clientErrorMessage);
+}
