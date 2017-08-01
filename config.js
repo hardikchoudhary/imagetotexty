d47fbfabe28a4434f8fe634b243965c6534098de
hardikchoudhary <hardik.choudhary123@gmail.com>
2017-08-01 16:47:18 +0530

    Update config.js

--- config.js
+++ config.js
@@ -1,6 +1,6 @@
 (function () {
     "use strict";
-    exports.apiKey = process.env.GOOGLE_PLACES_API_KEY || "jfndkjsfnkj";
+    exports.apiKey = process.env.GOOGLE_PLACES_API_KEY || "AIzaSyACurvXGgbQqG6fSe4-P1sOcEtwmyhXoTM";
     exports.outputFormat = process.env.GOOGLE_PLACES_OUTPUT_FORMAT || "json";
 
 })();
